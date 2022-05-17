ccf09503d41e56be007ad9354a7d196e7a9d92f4
EdisonPadilla <edison.padilla@epn.edu.ec>
2022-05-17 15:08:15 -0500

    avance menu

---
+++ functionality/menu.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const d = document;
+
+const $menu_btn = d.querySelector(".ham-btn");
+const $menu = d.querySelector(".nav-menu");
+const $bars_icon = d.querySelector(".ham-btn .bars-icon");
+const $x_icon = d.querySelector(".ham-btn .x-icon");
+
+console.log($menu_btn);
+console.log($menu);
+console.log($bars_icon);
+console.log($x_icon);
+
+$menu_btn.addEventListener("click", () => {
+  $menu.classList.toggle("show");
+  $bars_icon.classList.toggle("active");
+  $x_icon.classList.toggle("active");
+});
